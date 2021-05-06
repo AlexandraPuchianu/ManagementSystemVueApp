21ff46978951a2d9b22ad6e4a32dca1a01c271ce
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-06 13:04:32 +0300

    Added FormComponent body and style.

--- management-system-app/src/components/FormComponent.vue
+++ management-system-app/src/components/FormComponent.vue
@@ -1,5 +1,23 @@
 <template>
-    <p>Form comp</p>
+    <form id="form" onsubmit="return false">
+        <label>First name: </label>
+        <input type="text" id="fname" placeholder="First name"><br><br>
+        <label>Last name: </label>
+        <input type="text" id="lname" placeholder="Last Name"><br><br>
+        <label>Email: </label>
+        <input type="text" id="email" placeholder="Email"><br><br>
+        <label>Gender:</label>
+        <select id="gender">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+        </select><br><br>
+        <label>Birthdate: </label>
+        <input type="date" id="birthdate" value="2003-01-01" min="1921-01-01" max="2021-04-23"><br><br>
+        <label>Picture:</label>
+        <input type="file" id="picture" onchange="upload(this)"><br><br>
+        <button type="submit" class="submitButton" onclick="addEmployee()">Submit</button>
+    </form>
 </template>
 
 <script>
@@ -9,5 +27,22 @@ export default {
 </script>
 
 <style scoped>
+form{
+    margin-top: 3rem;
+    margin-left: 3rem;
+}
 
+.submitButton{
+    background-color: #1a73e8;
+    border: none;
+    color: white;
+    margin-left: 1.5rem;
+    text-decoration: none;
+    font-size: 14px;
+    cursor: pointer;
+    height: 2.3rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    border-radius: 4px;
+}
 </style>
