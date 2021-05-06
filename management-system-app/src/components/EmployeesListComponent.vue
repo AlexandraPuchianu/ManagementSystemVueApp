9df53549bcf531bec4b5ac3dd2173b6433b51500
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-06 13:21:32 +0300

    Added EmployeesListComponent body and style.

--- management-system-app/src/components/EmployeesListComponent.vue
+++ management-system-app/src/components/EmployeesListComponent.vue
@@ -1,5 +1,17 @@
 <template>
-    <p>Employees comp</p>
+    <div class="employeesList">
+        <table id="employeesTable">
+            <tr>
+                <th>Profil Picture</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Birthdate</th>
+                <th></th>
+            </tr>
+          </table>
+    </div>
 </template>
 
 <script>
@@ -9,5 +21,31 @@ export default {
 </script>
 
 <style scoped>
+#employeesList{
+    margin-top: 3rem;
+    margin-left: 3rem;
+}
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 80%;
+  }
+  
+  td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+  }
+  
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
 
+  #deleteBtn{
+    background-color: #1a73e8;
+    border: none;
+    color: white;
+    cursor: pointer;
+    font-size: 1rem;
+  }
 </style>
